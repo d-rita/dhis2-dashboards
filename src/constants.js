03a000b55d7e3e45892a7a05a6f6786abf196c2d
drita <drnanyanzi@gmail.com>
2023-11-11 00:43:31 +0300

    Fix dashboards url

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-export const DASHBOARDS_URL = 'https://gist.githubusercontent.com/kabaros/da79636249e10a7c991a4638205b1726/raw/fa044f54e7a5493b06bb51da40ecc3a9cb4cd3a5/dashboards.jsons';
+export const DASHBOARDS_URL = 'https://gist.githubusercontent.com/kabaros/da79636249e10a7c991a4638205b1726/raw/fa044f54e7a5493b06bb51da40ecc3a9cb4cd3a5/dashboards.json';
 
 export const DASHBOARD_DETAILS_URL = 'https://gist.githubusercontent.com/kabaros/da79636249e10a7c991a4638205b1726/raw/fa044f54e7a5493b06bb51da40ecc3a9cb4cd3a5/'; 
 
