421650494a6d284bc58c5babcecf93fe00016389
drita <drnanyanzi@gmail.com>
2023-11-11 04:08:26 +0300

    Add env file

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,2 @@
-export const DASHBOARDS_URL = 'https://gist.githubusercontent.com/kabaros/da79636249e10a7c991a4638205b1726/raw/fa044f54e7a5493b06bb51da40ecc3a9cb4cd3a5/dashboards.json';
-
-export const DASHBOARD_DETAILS_URL = 'https://gist.githubusercontent.com/kabaros/da79636249e10a7c991a4638205b1726/raw/fa044f54e7a5493b06bb51da40ecc3a9cb4cd3a5/'; 
-
+export const DASHBOARDS_URL = process.env.DASHBOARDS_URL || '';
+export const DASHBOARD_DETAILS_URL = process.env.DASHBOARD_DETAILS_URL || ''; 
