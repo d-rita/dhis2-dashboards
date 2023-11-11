1abb60328c78c8e61263e8489267c36ec90789dd
drita <drnanyanzi@gmail.com>
2023-11-11 04:57:21 +0300

    Import variables from constants file

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
-export const DASHBOARDS_URL = process.env.DASHBOARDS_URL || "";
-export const DASHBOARD_DETAILS_URL = process.env.DASHBOARD_DETAILS_URL || "";
+export const DASHBOARDS_URL = process.env.REACT_APP_DASHBOARDS_URL;
+export const DASHBOARD_DETAILS_URL = process.env.REACT_APP_DASHBOARD_DETAILS_URL; 
